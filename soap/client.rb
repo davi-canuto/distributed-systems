5ff24ddeb99e63a3772330f0d27ac7508546027d
davi-canuto <davicanutogregorio@gmail.com>
2023-11-14 11:39:13 -0300

    improvement: start works in javascript client and server package json

--- soap/client.rb
+++ soap/client.rb
@@ -2,9 +2,7 @@ require 'savon'
 
 soap_client = Savon.client(wsdl: 'http://service.example.com/wsdl?wsdl')
 
-# list operations
 soap_client.operations
 
-response = client.call(:find_user, message: { id: 42 })
-
+response = soap_client.call(:find_user, message: { id: 42 })
 response.body
