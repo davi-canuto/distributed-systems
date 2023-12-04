13c3d2b342be8ccb59eb9200ebf561f86c5be771
davi-canuto <davicanutogregorio@gmail.com>
2023-12-03 22:08:15 -0300

    fix: fixes cors and create index html event

--- rest/api_gateway/app.js
+++ rest/api_gateway/app.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 const app = express();
 const port = 3000;
 const catsUrl = "https://api.thecatapi.com/v1/images/search";
 const memesUrl = "https://api.imgflip.com/get_memes";
 
+app.use(cors());
+
 app.get("/cat", async (req, res) => {
   try {
     const response = await axios.get(catsUrl);
 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, GET, PUT, DELETE, OPTIONS"
+    );
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     res.send(response.data[0]);
   } catch (error) {
     res.send(error);
@@ -28,6 +38,20 @@ app.get("/meme", async (req, res) => {
     res.send(error);
   }
 });
+
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "API Gateway",
+//       version: "1.0.0",
+//     },
+//   },
+//   apis: ["./seu-arquivo-com-comentarios.js"],
+// };
+
+// const swaggerSpec = swaggerJsdoc(options);
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
