611bd5b7fb273eda2464d0dc8a3c1d360d3b0730
davi-canuto <davicanutogregorio@gmail.com>
2023-12-03 21:40:45 -0300

    improvement: create endpoints for get cats and memes

--- rest/api_gateway/app.js
+++ rest/api_gateway/app.js
@@ -1,9 +1,32 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = 3000;
+const catsUrl = "https://api.thecatapi.com/v1/images/search";
+const memesUrl = "https://api.imgflip.com/get_memes";
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/cats", async (req, res) => {
+  try {
+    const response = await axios.get(catsUrl);
+
+    res.send(response.data[0]);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.get("/memes", async (req, res) => {
+  try {
+    const response = await axios.get(memesUrl);
+
+    const items = response.data.data.memes;
+
+    var randomObject = items[Math.floor(Math.random() * items.length)];
+
+    res.send(randomObject);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.listen(port, () => {
