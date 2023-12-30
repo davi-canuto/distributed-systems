cbe4a16dbba5d8deb712526624f494cfbb8fca99
Davi Canuto <davialessandro33@gmail.com>
2023-12-30 11:09:43 -0300

    feat: add websocket chat

---
+++ web_socket/index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const staticHandler = require('serve-handler');
+const WebSocket = require('ws');
+
+// Servir pasta estática
+const server = http.createServer((req, res) => {
+  return staticHandler(req, res, { public: 'public' });
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (client) => {
+  console.log('Client connected!');
+  client.on('message', (msg) => {
+    console.log(`Message: ${msg}`);
+    broadcast(msg);
+  });
+});
+
+function broadcast(msg) {
+  for (const client of wss.clients) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(msg);
+    }
+  }
+}
+
+server.listen(process.argv[2] || 8080, '127.0.0.1', () => {
+  // http://127.0.0.1:8080/
+  console.log('Server listening...');
+});
